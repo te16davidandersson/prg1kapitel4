fa9b9bd064e91114e02f30df084aa8240991dbc6
David Andersson <anda16@nti.umea>
2017-10-03 09:25:15 +0200

    ovn4_3

--- ovn4_3.java
+++ ovn4_3.java
@@ -12,9 +12,13 @@ public class ovn4_3{
 
 		String betygg = JOptionPane.showInputDialog(null, "Hur m" + a1 + "nga po" + a2 + "ng fick du?");
 		int betyg = Integer.parseInt(betygg);
+			
 		
 
-		if (betyg >=45) {
+		if (betyg >50) {
+			JOptionPane.showMessageDialog(null, "Ange en siffra mellan 0 och 50.");
+
+		} else if (betyg >=45) {
 			JOptionPane.showMessageDialog(null, "Betyg: A");
 
 		} else if (betyg >=40) {
@@ -31,9 +35,6 @@ public class ovn4_3{
 
 		} else if (betyg <=24) {
 			JOptionPane.showMessageDialog(null, "Betyg: F");
-
-		} else {
-			JOptionPane.showMessageDialog(null, "Ange en siffra mellan 0 och 50.");
 		}
 	}
 }
